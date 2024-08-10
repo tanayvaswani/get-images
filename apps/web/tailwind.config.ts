55cb5e26ffc67150b2e75dbb4fba98874070cd5d
tanayvaswani <vaswani.tanay9@gmail.com>
2024-08-10 16:51:35 +0530

    feat: nextjs client

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,1 +1,20 @@
-module.exports = require("@repo/tailwind-config/tailwind.config");
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
